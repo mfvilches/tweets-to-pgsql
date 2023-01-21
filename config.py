6de4616cea84b43c71be60b303a2eabb4a77cd21
Mario Vilchesgit config --global user.name Mario <mvilchesp@udec.cl>
2023-01-21 07:55:29 -0300

    new file:   config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+#Archivo de configuracion
+
+dia = 16 # dia en que termina la extracción
+mes = 1 #mes de la extracción
+año = 2023 #año de la extracción
+ndias = 6 #dias anteriores a la extracción
+
+conn_string = 'postgresql://user:useradmin@255.255.255.255/bbdd'
