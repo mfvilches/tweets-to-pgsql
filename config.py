28d42adc4d06b517241ca55cc0f981dbe67d4028
Mario Vilches <mvilchesp@udec.cl>
2023-08-09 20:44:00 -0400

    modified:   config.py

--- config.py
+++ config.py
@@ -3,6 +3,6 @@
 dia = 16 # dia en que termina la extracción
 mes = 1 #mes de la extracción
 año = 2023 #año de la extracción
-ndias = 6 #dias anteriores a la extracción
+ndias = 7 #dias anteriores a la extracción
 
 conn_string = 'postgresql://user:useradmin@255.255.255.255/bbdd'
